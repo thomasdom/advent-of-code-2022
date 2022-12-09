29420bd72960d46b5d9d226e70ae73a72cb91a98
thomasdom <thomasdom@users.noreply.github.com>
2022-12-09 15:25:00 +0100

    finish day 04 part 2

--- day04/src/bonus.mjs
+++ day04/src/bonus.mjs
@@ -1,9 +1,19 @@
 import { readFile } from 'node:fs/promises';
+import { EOL } from 'node:os';
 import { resolve } from 'node:path';
+import { intersection, range } from 'lodash-es';
 async function main() {
-  const input = await readFile(resolve('input/EDIT_ME.txt'), {
+  const input = await readFile(resolve('input/sections.txt'), {
     encoding: 'utf8',
   });
+  const ranges = input
+    .split(EOL)
+    .map((assignment) => assignment.split(','))
+    .map((assignment) => assignment.map((a) => a.split('-')))
+    .map((assignment) => assignment.map((a) => range(Number(a[0]), Number(a[1]) + 1)));
+  const overlaps = ranges.filter((r) => intersection(...r).length);
+
+  console.log(`The number of overlaps is ${overlaps.length}.`);
 }
 
 main().catch(console.error);
