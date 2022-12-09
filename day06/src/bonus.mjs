7049c96ad574617da8b2a794190b2d98992803e8
thomasdom <thomasdom@users.noreply.github.com>
2022-12-09 17:57:00 +0100

    finish day 06 part 2

--- day06/src/bonus.mjs
+++ day06/src/bonus.mjs
@@ -1,9 +1,19 @@
 import { readFile } from 'node:fs/promises';
 import { resolve } from 'node:path';
+import { uniq } from 'lodash-es';
+
+const MARKER_LENGTH = 14;
 async function main() {
-  const input = await readFile(resolve('input/EDIT_ME.txt'), {
+  const input = await readFile(resolve('input/signal.txt'), {
     encoding: 'utf8',
   });
+  const signal = input.trim().trim('\n');
+  let position = 0;
+  while (uniq(signal.substr(position, MARKER_LENGTH).split('')).length !== MARKER_LENGTH) {
+    position++;
+  }
+  let startOfPacketPosition = position + MARKER_LENGTH;
+  console.log(`The start-of-packet marker is at position ${startOfPacketPosition}.`);
 }
 
 main().catch(console.error);
