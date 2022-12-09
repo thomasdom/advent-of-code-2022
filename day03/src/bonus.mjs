547acaf116c70ea5441efaea54159099d11b490a
thomasdom <thomasdom@users.noreply.github.com>
2022-12-09 14:54:06 +0100

    finish day 03 part 2

--- day03/src/bonus.mjs
+++ day03/src/bonus.mjs
@@ -1,9 +1,24 @@
-import { readFile } from 'node:fs/promises';
-import { resolve } from 'node:path';
+import { readFile } from 'fs/promises';
+import { EOL } from 'os';
+import { resolve } from 'path';
+import { chunk, intersection, sum } from 'lodash-es';
+
+const calculatePriorityForItem = (item) => {
+  if (/[a-z]/.test(item)) {
+    return item.charCodeAt(0) - 96;
+  }
+  return item.charCodeAt(0) - 38;
+};
 async function main() {
-  const input = await readFile(resolve('input/EDIT_ME.txt'), {
+  const input = await readFile(resolve('input/rucksacks.txt'), {
     encoding: 'utf8',
   });
+  const rucksacks = input.split(EOL);
+  const groups = chunk(rucksacks, 3);
+  const groupBadges = groups.map((group) => intersection(...group.map((g) => g.split('')))[0]);
+  const sumOfPriorities = sum(groupBadges.map(calculatePriorityForItem));
+
+  console.log(`The sum of priorities for rucksack groups is ${sumOfPriorities}.`);
 }
 
 main().catch(console.error);
