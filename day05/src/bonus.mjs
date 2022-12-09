39ea923fd5e46e5e25d0d809f97da7b6a77ccbf6
thomasdom <thomasdom@users.noreply.github.com>
2022-12-09 17:03:05 +0100

    lint code

--- day05/src/bonus.mjs
+++ day05/src/bonus.mjs
@@ -1,7 +1,7 @@
 import { readFile } from 'node:fs/promises';
 import { EOL } from 'node:os';
 import { resolve } from 'node:path';
-import {compact, concat, dropRight, initial, last, tail, takeRight, trim, zip} from 'lodash-es';
+import { compact, concat, dropRight, initial, last, tail, takeRight, trim, zip } from 'lodash-es';
 
 async function main() {
   const input = await readFile(resolve('input/stacks.txt'), {
